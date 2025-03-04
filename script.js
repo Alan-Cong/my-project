38fd802b3f3dda939069a5345b57bbcbd763ecab
Alan-Cong <3302932711@qq.com>
2025-03-04 13:26:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// Show More Information on Button Click
-function showMoreInfo() {
-    alert("项目展示包括关于团队、进展、成就与影响等内容。感谢您的关注!");
-}
+// 当页面加载完成后，再给按钮绑定交互逻辑
+document.addEventListener("DOMContentLoaded", () => {
+    // showMoreInfo函数：点击“了解更多”按钮后执行
+    window.showMoreInfo = function() {
+        alert("感谢关注本项目！我们致力于通过影像和设计赋能乡村电商，助力品牌升级。");
+    };
+});
